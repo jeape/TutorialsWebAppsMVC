2aa18becb5925db992f9a68f64534ba0078aad96
Jeanette <jeape@outlook.dk>
2021-12-10 15:31:05 +0100

    feat: Add a New Field: Include rating field in initial seeding of DB.

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -28,7 +28,8 @@ namespace MovieApp.Models
                         Title = "When Harry Met Sally",
                         ReleaseDate = DateTime.Parse("1989-2-12"),
                         Genre = "Romantic Comedy",
-                        Price = 7.99M
+                        Price = 7.99M,
+                        Rating = "A"
                     },
 
                     new Movie
@@ -36,7 +37,8 @@ namespace MovieApp.Models
                         Title = "Ghostbusters ",
                         ReleaseDate = DateTime.Parse("1984-3-13"),
                         Genre = "Comedy",
-                        Price = 8.99M
+                        Price = 8.99M,
+                        Rating = "B"
                     },
 
                     new Movie
@@ -44,7 +46,8 @@ namespace MovieApp.Models
                         Title = "Ghostbusters 2",
                         ReleaseDate = DateTime.Parse("1986-2-23"),
                         Genre = "Comedy",
-                        Price = 9.99M
+                        Price = 9.99M,
+                        Rating = "C"
                     },
 
                     new Movie
@@ -52,7 +55,9 @@ namespace MovieApp.Models
                         Title = "Rio Bravo",
                         ReleaseDate = DateTime.Parse("1959-4-15"),
                         Genre = "Western",
-                        Price = 3.99M
+                        Price = 3.99M,
+                        Rating = "D"
+
                     }
                 );
                 context.SaveChanges();
